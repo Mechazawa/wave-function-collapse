1d1ca62cc6832aef879689fd001eba50c3dad6a7
shodan <mega@ioexception.at>
2023-04-07 04:52:01 +0200

    attempt

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,24 @@
 mod lib;
 
-use image::{ImageError, GenericImageView};
+use image::{ImageError, RgbaImage};
 use image::{io::Reader as ImageReader, DynamicImage};
-use log::{debug, info};
+use log::{debug, info, trace};
+use rand::thread_rng;
 use simplelog::{ColorChoice, Config, TermLogger, TerminalMode};
+use std::collections::{HashMap, HashSet};
 use std::fmt::Debug;
 use std::path::PathBuf;
+use std::rc::Rc;
 use structopt::StructOpt;
 use structopt_flags::{LogLevel, QuietVerbose};
+use image::GenericImageView;
+use rand::prelude::SliceRandom;
 
-use lib::Tile;
+use lib::Collapsable;
+use lib::Direction;
 use lib::Size;
+use lib::SuperState;
+use lib::Tile;
 
 fn load_image(s: &str) -> Result<DynamicImage, ImageError> {
     let path = PathBuf::from(s);
@@ -31,8 +39,8 @@ struct Opt {
     #[structopt(parse(try_from_str=load_image), help = "Input image")]
     input: DynamicImage,
 
-    #[structopt(parse(try_from_str=load_image), help = "Output image")]
-    ouput: Option<DynamicImage>,
+    #[structopt(parse(from_os_str), help = "Output image")]
+    output: PathBuf,
 
     #[structopt(
         parse(try_from_str),
@@ -67,4 +75,63 @@ fn main() {
     let tiles = Tile::get_tile_set(&opt.input, &opt.input_size);
 
     info!("{} unique tiles found", tiles.len());
+
+    let base_state = SuperState {
+        possible: tiles.clone(),
+    };
+
+    let mut grid = vec![base_state.clone(); opt.output_size.area() as usize];
+
+    let mut stack: Vec<usize> = (0..grid.len()).collect();
+    let mut rng = thread_rng();
+    let (image_width, image_height) = opt.input.dimensions();
+    let tile_width = image_width / opt.input_size.width;
+    let tile_height = image_height / opt.input_size.height;
+    let mut canvas = RgbaImage::new(
+        opt.output_size.width * tile_width, 
+        opt.output_size.height * tile_height,
+    );
+
+    stack.shuffle(&mut rng);
+
+    grid[stack.pop().unwrap()].collapse(&mut rng);
+
+    while stack.len() > 0 {
+        info!("stack {}", stack.len());
+
+        // todo: optimise to only test top x positions
+        // test all positions
+        for index in stack.iter() {
+            let mut neighbors: HashMap<Direction, Vec<Rc<Tile>>> = HashMap::new();
+
+            for (direction, offset) in opt.output_size.get_offsets() {
+                let target = (*index) as i32 + offset;
+                
+                if let Some(cell) = grid.get(target as usize) {
+                    neighbors.insert(direction, cell.possible.clone());
+                }
+            }
+
+            trace!("{}", index);
+            grid[*index].tick(&neighbors);
+        }
+
+        // sort the stack; entropy ascending
+        stack.sort_by(|a, b| grid[*b].entropy().cmp(&grid[*a].entropy()));
+
+        // draw
+        // todo only draw recently collapsed
+        // todo broken :(
+        for index in 0..grid.len() {
+            let x = index as u32 % opt.input_size.width;
+            let y = index as u32 / opt.input_size.width;
+
+            if let Some(t) = grid[index].collapsed() {
+                trace!("draw {}, {}", x, y);
+                image::imageops::overlay(&mut canvas, t.image.as_ref(), (x * tile_width) as i64, (y * tile_height) as i64);
+            }
+        }
+
+        canvas.save(opt.output.as_path()).unwrap();
+    }
 }
